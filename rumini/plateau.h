df5469696205eb330909f8afd1fbbe2ae4f58c5a
arobaseSuulei <souleymanediallo2004@outlook.com>
2025-12-25 14:42:12 +0100

    numerotes les tuiles

---
+++ rumini/plateau.h
@@ -0,0 +1,15 @@
+// les guards --> permettent la définition qu'une seule fois
+#ifndef PLATEAU_H 
+#define PLATEAU_H
+
+#include "struct.h"
+#include "Tuile.h"
+#include "plateau.c"
+
+
+
+//void deposer_tuile(Tuile );
+
+
+
+#endif
